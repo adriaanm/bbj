3c7bed9f50922f0f2b533feb081b4e6986ac982b
Adriaan Moors <adriaan.moors@typesafe.com>
2012-12-27 00:10:04 -0800

    clean up issue loading and caching

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,6 +12,12 @@ object Application extends Controller {
   import play.api.libs.concurrent.Execution.Implicits._
   import util.jira.rest.api
 
+  def renderFailure(t: Throwable) = t match {
+    case e: NoSuchElementException => <div></div>
+    case _ => <div class="error">{t.getMessage}</div>
+  }
+
+
   def renderIssue(issue: Issue) =
     <div> {issue.key}: <ul>{ issue.fields.toList.map {
         case (k@("votes" | "watches"), v: Future[List[User]]) => <li> {k}: {Await.result(v, Duration.Inf)} </li>
@@ -20,12 +26,10 @@ object Application extends Controller {
     </div>
 
   def index = Action {
-    Async {
-      val renderFutures = api.issues.map(x => x.map(renderIssue))
-      Future.sequence(renderFutures).map { renderedIssues =>
-        Ok(views.html.index(renderedIssues.map(identity)(collection.breakOut)))
-      }
-    }
+    val res: Future[xml.NodeSeq] =
+      api.issues.map { _.map(renderIssue)(collection.breakOut) }
+
+    Async{res map {ns: xml.NodeSeq => Ok(views.html.index(ns))}}
   }
 
 }
--- app/util/jira/rest/Api.scala
+++ app/util/jira/rest/Api.scala
@@ -17,13 +17,15 @@ import java.io.FileOutputStream
 import scala.collection.mutable.ArrayBuffer
 
 trait JIRAConnection {
+  def getString(x: String) = play.api.Play.current.configuration.getString(x)
+
   // try to authorize, or fall back to non-auth
   // need to authorize to get voters & watchers, apparently...
   def tryAuthUrl(url: String) = {
-    val getString = play.api.Play.current.configuration.getString(_: String)
-    (for (user <- getString("jira.user");
-         pass <- getString("jira.password"))
-      yield WS.url(url).withAuth(user, pass, com.ning.http.client.Realm.AuthScheme.BASIC)).getOrElse(WS.url(url))
+    (for (
+      user <- getString("jira.user");
+      pass <- getString("jira.password")
+    ) yield WS.url(url).withAuth(user, pass, com.ning.http.client.Realm.AuthScheme.BASIC)).getOrElse(WS.url(url))
   }
 }
 
@@ -36,22 +38,22 @@ trait Validation extends JIRAConnection {
 
   def optField(x: JsValue)(name: String): Option[JsValue] = x match {
     case o: JsObject => o.value.get(name)
-    case _ => None
+    case _           => None
   }
 
   def name(json: JsValue) = (json \ "name").asOpt[String]
   def self(json: JsValue) = (json \ "self").asOpt[String]
-  def id(json: JsValue)   = (json \ "id").asOpt[String] map Integer.parseInt
+  def id(json: JsValue) = (json \ "id").asOpt[String] map Integer.parseInt
 
   def lazyList[T](json: JsValue, countField: String, elemsField: String)(implicit creator: Reads[T]): Future[List[T]] = Future.successful(Nil)
-//    if ((json \ countField).as[Int] == 0) Future.successful(Nil)
-//    else {
-//      import play.api.libs.concurrent.Execution.Implicits._
-//      val self = (json \ "self").as[String]
-//      tryAuthUrl(self).get().map{ req =>
-//        (req.json \ elemsField).as[List[T]]
-//      }
-//    }
+  //    if ((json \ countField).as[Int] == 0) Future.successful(Nil)
+  //    else {
+  //      import play.api.libs.concurrent.Execution.Implicits._
+  //      val self = (json \ "self").as[String]
+  //      tryAuthUrl(self).get().map{ req =>
+  //        (req.json \ elemsField).as[List[T]]
+  //      }
+  //    }
 }
 
 object User extends Validation {
@@ -74,9 +76,9 @@ class User(val self: String, val name: String) {
 object Status extends Validation {
   implicit object reads extends Reads[Status] {
     def reads(json: JsValue): JsResult[Status] = validate(s"status; got $json")(for (
-      self        <- self(json);
-      id          <- id(json);
-      name        <- name(json);
+      self <- self(json);
+      id <- id(json);
+      name <- name(json);
       description <- (json \ "description").asOpt[String]
     ) yield Status(self, id, name, description))
   }
@@ -85,16 +87,16 @@ object Status extends Validation {
   def apply(self: String, id: Int, name: String, description: String) =
     stati.getOrElseUpdate(self, new Status(self, id, name, description))
 }
-class Status(val self: String, val id: Int, val name: String, val description: String)  {
+class Status(val self: String, val id: Int, val name: String, val description: String) {
   override def toString = name
 }
 
 object Resolution extends Validation {
   implicit object reads extends Reads[Resolution] {
     def reads(json: JsValue): JsResult[Resolution] = validate(s"resolution; got $json")(for (
-      self        <- self(json);
-      id          <- id(json);
-      name        <- name(json);
+      self <- self(json);
+      id <- id(json);
+      name <- name(json);
       description <- (json \ "description").asOpt[String]
     ) yield Resolution(self, id, name, description))
   }
@@ -103,20 +105,20 @@ object Resolution extends Validation {
   def apply(self: String, id: Int, name: String, description: String) =
     resolutions.getOrElseUpdate(self, new Resolution(self, id, name, description))
 }
-class Resolution(val self: String, val id: Int, val name: String, val description: String)  {
+class Resolution(val self: String, val id: Int, val name: String, val description: String) {
   override def toString = name
 }
 
 object Version extends Validation {
   implicit object reads extends Reads[Version] {
     def reads(json: JsValue): JsResult[Version] = validate(s"version; got $json")(for (
-      self            <- self(json);
-      id              <- id(json);
-      name            <- name(json);
+      self <- self(json);
+      id <- id(json);
+      name <- name(json);
       userReleaseDate <- (optField(json)("userReleaseDate")).map(_.asOpt[String]).orElse(Some(None));
-      releaseDate     <- (optField(json)("releaseDate")).map(_.asOpt[Date](Reads.DefaultDateReads)).orElse(Some(None));
-      archived        <- (json \ "archived").asOpt[Boolean];
-      released        <- (json \ "released").asOpt[Boolean]
+      releaseDate <- (optField(json)("releaseDate")).map(_.asOpt[Date](Reads.DefaultDateReads)).orElse(Some(None));
+      archived <- (json \ "archived").asOpt[Boolean];
+      released <- (json \ "released").asOpt[Boolean]
     ) yield new Version(self, id, name, userReleaseDate, releaseDate, archived, released))
   }
 
@@ -124,18 +126,18 @@ object Version extends Validation {
   def apply(self: String, id: Int, name: String, userReleaseDate: Option[String], releaseDate: Option[Date], archived: Boolean, released: Boolean) =
     versions.getOrElseUpdate(self, new Version(self, id, name, userReleaseDate, releaseDate, archived, released))
 }
-class Version(val self: String, val id: Int, val name: String, val userReleaseDate: Option[String], val releaseDate: Option[Date], val archived: Boolean, val released: Boolean)  {
+class Version(val self: String, val id: Int, val name: String, val userReleaseDate: Option[String], val releaseDate: Option[Date], val archived: Boolean, val released: Boolean) {
   override def toString = name
 }
 
 object Comment extends Validation {
   implicit object reads extends Reads[Comment] {
     def reads(json: JsValue): JsResult[Comment] = validate(s"comment; got $json")(for (
-      author       <- (json \ "author").asOpt[User];
-      body         <- (json \ "body").asOpt[String];
+      author <- (json \ "author").asOpt[User];
+      body <- (json \ "body").asOpt[String];
       updateAuthor <- (json \ "updateAuthor").asOpt[User];
-      created      <- (json \ "created").asOpt[Date];
-      updated      <- (json \ "updated").asOpt[Date]
+      created <- (json \ "created").asOpt[Date];
+      updated <- (json \ "updated").asOpt[Date]
     ) yield Comment(author, body, updateAuthor, created, updated))
   }
 }
@@ -146,13 +148,13 @@ case class Comment(author: User, body: String, updateAuthor: User, created: Date
 object Attachment extends Validation {
   implicit object reads extends Reads[Attachment] {
     def reads(json: JsValue): JsResult[Attachment] = validate(s"attachment; got $json")(for (
-      filename   <- (json \ "filename").asOpt[String];
-      author     <- (json \ "author").asOpt[User];
-      created    <- (json \ "created").asOpt[Date];
-      size       <- (json \ "size").asOpt[Int];
-      mimeType   <- (json \ "mimeType").asOpt[String];
+      filename <- (json \ "filename").asOpt[String];
+      author <- (json \ "author").asOpt[User];
+      created <- (json \ "created").asOpt[Date];
+      size <- (json \ "size").asOpt[Int];
+      mimeType <- (json \ "mimeType").asOpt[String];
       properties <- (optField(json)("properties")).map(_.asOpt[JsObject]).orElse(Some(None));
-      content    <- (json \ "content").asOpt[String]
+      content <- (json \ "content").asOpt[String]
     ) yield Attachment(filename, author, created, content, size, mimeType, properties))
   }
 }
@@ -161,15 +163,17 @@ case class Attachment(filename: String, author: User, created: Date, content: St
 object IssueLink extends Validation {
   implicit object reads extends Reads[IssueLink] {
     def reads(json: JsValue): JsResult[IssueLink] = validate(s"issue link; got $json")(for (
-      name    <- name(json \ "type");
-      inward  <- (json \ "type" \ "inward").asOpt[String];
+      name <- name(json \ "type");
+      inward <- (json \ "type" \ "inward").asOpt[String];
       outward <- (json \ "type" \ "outward").asOpt[String];
-      outwardIssue <- (for(
-          out <- (optField(json)("outwardIssue"));
-          out <- out.asOpt[JsObject]) yield (out \ "key").asOpt[String]).orElse(Some(None));
-      inwardIssue <- (for(
-          in  <- (optField(json)("inwardIssue"));
-          in  <- in.asOpt[JsObject]) yield (in \ "key").asOpt[String]).orElse(Some(None))
+      outwardIssue <- (for (
+        out <- (optField(json)("outwardIssue"));
+        out <- out.asOpt[JsObject]
+      ) yield (out \ "key").asOpt[String]).orElse(Some(None));
+      inwardIssue <- (for (
+        in <- (optField(json)("inwardIssue"));
+        in <- in.asOpt[JsObject]
+      ) yield (in \ "key").asOpt[String]).orElse(Some(None))
     ) yield IssueLink(name, outward, outwardIssue, inward, inwardIssue))
   }
 }
@@ -178,96 +182,87 @@ case class IssueLink(name: String, outward: String, outwardIssue: Option[String]
 object api extends Validation {
   import play.api.libs.concurrent.Execution.Implicits._
 
-  private val jsonRepo = "/Users/adriaan/jira"
-  private def jiraUrl(uri: String) = "https://issues.scala-lang.org/rest/api/latest" + uri
-  private def downloadIssue(i: Int) = tryAuthUrl(jiraUrl(s"/issue/SI-${i}?expand=changelog")).get()
-
-  // write the inputStream to a FileOutputStream and make sure the recevied Json parses (sometimes the server returns garbage!?)
-  private def copyJsonToFile(in: InputStream, f: File, bufferSize: Int = 1024) = /*f.synchronized*/ {
+  private def readFully(in: InputStream, bufferSize: Int = 1024): Array[Byte] = {
     val bytes = new Array[Byte](bufferSize)
-    val out = new FileOutputStream(f)
     val contents = ArrayBuffer[Byte]()
 
     @annotation.tailrec
-    def write(): Unit =
+    def read(): Unit =
       in.read(bytes) match {
         case -1 =>
         case count =>
-          val read = new Array[Byte](count)
-          Array.copy(bytes, 0, read, 0, read.size)
-          contents ++= read
-          out.write(bytes, 0, count)
-          write()
+          val data = new Array[Byte](count)
+          Array.copy(bytes, 0, data, 0, count)
+          contents ++= data
+          read()
       }
 
-    try { write(); Json.parse(new String(contents.toArray)) }
-    catch { case e =>
-      println("FILE EXCEPTION: "+ e)
-      f.delete()
-      throw e
-    }
-    finally { out.flush(); out.close() }
+    read()
+    contents.toArray
   }
 
-  // this needs to run until fixpoint...
-  // TODO:
-  // - convert into proper monadic computation that tracks whether something was changed and repeat
-  // - do twice and make sure the contents don't change --> use git repo?
-  private def tryDownloadIssues(issues: List[Int]): List[Future[Option[Int]]] =
-    issues.map { i =>
-      try {
-        val f = new File(s"$jsonRepo/${i}.json")
-        if (f.exists) Future.successful(None) // assume corrupted files are deleted by copyToFile
-        else downloadIssue(i).map { resp =>
-          copyJsonToFile(resp.ahcResponse.getResponseBodyAsStream, f)
-          println("YEP: "+ i)
-          None
-        } recover { case e =>
-          println(s"MEH: $i with $e")
-          Some(i)
+  private lazy val cacheDir = {
+    val cacheDir = getString("jira.cacheDir") getOrElse "/Users/adriaan/jira"
+    assert(new File(cacheDir).isDirectory(), s"Please create cache directory $cacheDir to avoid hammering the jira server.")
+    cacheDir
+  }
+
+  private def fileFor(key: Int) = new File(s"$cacheDir/${key}.json")
+  private def jiraUrl(uri: String) = "https://issues.scala-lang.org/rest/api/latest" + uri
+
+  def getIssue(i: Int): Future[Option[Issue]] = {
+    def loadCachedIssue =
+      Future (Some {
+        val f = fileFor(i)
+        val data = new Array[Byte](f.length.asInstanceOf[Int])
+        (new DataInputStream(new FileInputStream(f))).readFully(data)
+        parseIssue(data)
+      })
+
+    def downloadIssueAndCache =
+      tryAuthUrl(jiraUrl(s"/issue/SI-${i}?expand=changelog")).get().map { resp =>
+        val contents = readFully(resp.ahcResponse.getResponseBodyAsStream)
+        try Some(parseIssue(contents)) // don't bother writing to disk if it doesn't parse
+        finally {
+          val out = new FileOutputStream(fileFor(i))
+          try out.write(contents, 0, contents.size)
+          finally { out.flush(); out.close() }
+          println("YEP: " + i)
         }
-      } catch {
-        case e: Exception =>
-          println(s"$i failed: $e")
-          Future.successful(Some(i))
-      }
-    }
-
-  private def downloadIssues(): Future[Unit]  = {
-    def loop(failed: List[Int]): Future[Unit] =
-      Future.sequence(tryDownloadIssues(failed)).map(_.flatten).flatMap {
-        case Nil => Future.successful(())
-        case failed =>
-          println("retrying: "+ failed)
-          loop(failed)
       }
 
-    loop((firstIssueKey to lastIssueKey).toList)
-  }
-
-  def getIssue(key: String): Future[Issue] = //tryAuthUrl(jiraUrl(s"/issue/$key?expand=changelog")).get().map(req => parseIssue(req.json))
-    Future {
-      val f = new File(s"$jsonRepo/${key.substring(3)}.json") // drop "SI-" prefix
-      val data = new Array[Byte](f.length.asInstanceOf[Int])
-      (new DataInputStream(new FileInputStream(f))).readFully(data)
-      parseIssue(Json.parse(new String(data)))
+    loadCachedIssue recoverWith {
+      case _ => // if we couldn't load from disk, ask jira thrice
+        def download(retries: Int): Future[Option[Issue]] =
+          downloadIssueAndCache recoverWith {
+            case e if retries == 0 => throw e
+            // don't retry when server says the issue doesn't exist
+            case e: NoSuchElementException =>
+              println(s"issue does not exist: $i ($e)")
+              Future.successful(None)
+            case e =>
+              println(s"MEH: $i with $e")
+              download(retries - 1)
+          }
+        download(3)
     }
-
-  val firstIssueKey = 1
-  val lastIssueKey = 1000 // 6856
-  def issues: IndexedSeq[Future[Issue]] = {
-   val keys = (firstIssueKey to lastIssueKey)
-   if (keys.exists{i => !(new File(s"$jsonRepo/${i}.json")).exists})
-     Await.ready(downloadIssues(), Duration.Inf )
-
-   keys.map (i => getIssue("SI-"+i) recover { case e: Exception =>
-     e.printStackTrace()
-     Issue("SI-"+i, Map("!!!exception" -> e), Nil)
-   })
   }
 
+  lazy val issues: Future[IndexedSeq[Issue]] =
+    Future.sequence { (1 to 6882).map { getIssue } }.map(_.flatten)
+
+  def parseIssue(data: Array[Byte]): Issue = parseIssue(Json.parse(new String(data)))
   def parseIssue(i: JsValue): Issue =
-    Issue((i \ "key").as[String], (i \ "fields").as[Map[String, JsValue]].map { case (k, v) => parseField(k, v) }, (i \ "changelog" \ "histories").as[List[JsValue]])
+    optField(i)("errorMessages").map(_.as[List[String]]) match {
+      case Some(errorMessages) =>
+        if (errorMessages contains "Issue Does Not Exist") throw new NoSuchElementException(errorMessages.mkString("\n"))
+        else throw new IllegalArgumentException(errorMessages.mkString("\n"))
+      case None =>
+        Issue(
+          (i \ "key").as[String],
+          (i \ "fields").as[Map[String, JsValue]].map { case (k, v) => parseField(k, v) },
+          (i \ "changelog" \ "histories").as[List[JsValue]])
+    }
 
   def parseField(field: String, v: JsValue): (String, Any) = (field,
     try field match {
@@ -302,8 +297,9 @@ object api extends Validation {
       case "customfield_10101" => v.asOpt[List[String]] // flagged
       case "customfield_10104" => v.asOpt[Float] // Story points
       case "customfield_10105" => v.asOpt[Float] // business value
-      case "subtasks"          => assert(v.as[List[JsValue]].isEmpty, "subtasks not supported"); v
-      case "workratio"         => v.asOpt[Float]
+      case "subtasks" =>
+        assert(v.as[List[JsValue]].isEmpty, "subtasks not supported"); v
+      case "workratio" => v.asOpt[Float]
     } catch {
       case e: Exception => throw new Exception(s"Error parsing field $field : $v", e)
     })

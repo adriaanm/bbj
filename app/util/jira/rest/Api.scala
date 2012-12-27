363fb1ed6cee58993948d4b04919730c276419c2
Adriaan Moors <adriaan.moors@typesafe.com>
2012-12-27 01:12:09 -0800

    cleanup + hash consing for Version

--- app/util/jira/rest/Api.scala
+++ app/util/jira/rest/Api.scala
@@ -16,7 +16,9 @@ import java.io.InputStream
 import java.io.FileOutputStream
 import scala.collection.mutable.ArrayBuffer
 
-trait JIRAConnection {
+trait Common {
+  implicit lazy val defaultContext: scala.concurrent.ExecutionContext = play.api.libs.concurrent.Execution.Implicits.defaultContext
+
   def getString(x: String) = play.api.Play.current.configuration.getString(x)
 
   // try to authorize, or fall back to non-auth
@@ -27,9 +29,7 @@ trait JIRAConnection {
       pass <- getString("jira.password")
     ) yield WS.url(url).withAuth(user, pass, com.ning.http.client.Realm.AuthScheme.BASIC)).getOrElse(WS.url(url))
   }
-}
 
-trait Validation extends JIRAConnection {
   //  2011-05-18T15:37:07.000+0200
   implicit val dateRead = Reads.dateReads("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
 
@@ -56,7 +56,7 @@ trait Validation extends JIRAConnection {
   //    }
 }
 
-object User extends Validation {
+object User extends Common {
   implicit object reads extends Reads[User] {
     def reads(json: JsValue): JsResult[User] = validate("user")(for (
       self <- self(json);
@@ -73,7 +73,7 @@ class User(val self: String, val name: String) {
   override def toString = name
 }
 
-object Status extends Validation {
+object Status extends Common {
   implicit object reads extends Reads[Status] {
     def reads(json: JsValue): JsResult[Status] = validate(s"status; got $json")(for (
       self <- self(json);
@@ -91,7 +91,7 @@ class Status(val self: String, val id: Int, val name: String, val description: S
   override def toString = name
 }
 
-object Resolution extends Validation {
+object Resolution extends Common {
   implicit object reads extends Reads[Resolution] {
     def reads(json: JsValue): JsResult[Resolution] = validate(s"resolution; got $json")(for (
       self <- self(json);
@@ -109,7 +109,12 @@ class Resolution(val self: String, val id: Int, val name: String, val descriptio
   override def toString = name
 }
 
-object Version extends Validation {
+/*
+issues.flatMap(_.fields("versions").asInstanceOf[List[Version]]).toSet
+Set(Scala 2.10.0-M4, Scala 2.8.1, Scala 2.10.0-M7, Scala 2.9.2, Scala 2.10.0, Scala 2.11.0, Scala 2.10.0-M3, Scala 2.10.0-RC5, Scala 2.9.0,
+    Scala 2.9.1, Scala 2.10.0-M5, Scala 2.9.3-RC1, Scala 2.10.0-RC2, Scala 2.7.7, Scala 2.9.0-1, Scala 2.10.0-M6, Scala 2.10.1-RC1, Scala 2.10.0-M2, Scala 2.10.0-M1, Scala 2.8.0, macro-paradise, Scala 2.11.0-M1, Scala 2.10.0-RC1, Scala 2.10.0-RC3)
+ */
+object Version extends Common {
   implicit object reads extends Reads[Version] {
     def reads(json: JsValue): JsResult[Version] = validate(s"version; got $json")(for (
       self <- self(json);
@@ -119,7 +124,7 @@ object Version extends Validation {
       releaseDate <- (optField(json)("releaseDate")).map(_.asOpt[Date](Reads.DefaultDateReads)).orElse(Some(None));
       archived <- (json \ "archived").asOpt[Boolean];
       released <- (json \ "released").asOpt[Boolean]
-    ) yield new Version(self, id, name, userReleaseDate, releaseDate, archived, released))
+    ) yield Version(self, id, name, userReleaseDate, releaseDate, archived, released))
   }
 
   private val versions = collection.mutable.HashMap[String, Version]()
@@ -130,7 +135,7 @@ class Version(val self: String, val id: Int, val name: String, val userReleaseDa
   override def toString = name
 }
 
-object Comment extends Validation {
+object Comment extends Common {
   implicit object reads extends Reads[Comment] {
     def reads(json: JsValue): JsResult[Comment] = validate(s"comment; got $json")(for (
       author <- (json \ "author").asOpt[User];
@@ -145,7 +150,7 @@ case class Comment(author: User, body: String, updateAuthor: User, created: Date
   override def toString = s"on $created, $author said '$body' ($updated, $updateAuthor)"
 }
 
-object Attachment extends Validation {
+object Attachment extends Common {
   implicit object reads extends Reads[Attachment] {
     def reads(json: JsValue): JsResult[Attachment] = validate(s"attachment; got $json")(for (
       filename <- (json \ "filename").asOpt[String];
@@ -160,7 +165,18 @@ object Attachment extends Validation {
 }
 case class Attachment(filename: String, author: User, created: Date, content: String, size: Int, mimeType: String, properties: Option[JsObject])
 
-object IssueLink extends Validation {
+/**
+scala> issues.flatMap(_.fields("issuelinks").asInstanceOf[List[IssueLink]].map(_.name)).distinct
+Vector(Relates, Duplicate, Blocks, Cloners)
+
+scala> issues.flatMap(_.fields("issuelinks").asInstanceOf[List[IssueLink]].map(_.inward)).distinct
+Vector(relates to, is duplicated by, is blocked by, is cloned by)
+
+scala> issues.flatMap(_.fields("issuelinks").asInstanceOf[List[IssueLink]].map(_.outward)).distinct
+Vector(relates to, duplicates, blocks, clones)
+ *
+ */
+object IssueLink extends Common {
   implicit object reads extends Reads[IssueLink] {
     def reads(json: JsValue): JsResult[IssueLink] = validate(s"issue link; got $json")(for (
       name <- name(json \ "type");
@@ -177,11 +193,27 @@ object IssueLink extends Validation {
     ) yield IssueLink(name, outward, outwardIssue, inward, inwardIssue))
   }
 }
-case class IssueLink(name: String, outward: String, outwardIssue: Option[String], inward: String, inwardIssue: Option[String])
+case class IssueLink(name: String, outward: String, outwardIssue: Option[String], inward: String, inwardIssue: Option[String]) {
+  // assert:
+  (name, outward, inward) match {
+    case ("Relates", "relates to", "relates to") =>
+    case ("Duplicate", "is duplicated by", "duplicates") =>
+    case ("Duplicate", "duplicates", "is duplicated by") =>
+    case ("Blocks", "is blocked by", "blocks") =>
+    case ("Blocks", "blocks", "is blocked by") =>
+    case ("Cloners", "is cloned by", "clones") =>
+    case ("Cloners", "clones", "is cloned by") =>
+  }
+}
 
-object api extends Validation {
-  import play.api.libs.concurrent.Execution.Implicits._
+/** to experiment from the play console:
 
+new play.core.StaticApplication(new java.io.File("."))
+import util.jira.rest._
+val issues = concurrent.Await.result(api.issues, concurrent.duration.Duration.Inf)
+
+ */
+object api extends Common {
   private def readFully(in: InputStream, bufferSize: Int = 1024): Array[Byte] = {
     val bytes = new Array[Byte](bufferSize)
     val contents = ArrayBuffer[Byte]()
@@ -241,7 +273,8 @@ object api extends Validation {
               println(s"issue does not exist: $i ($e)")
               Future.successful(None)
             case e =>
-              println(s"MEH: $i with $e")
+              println(s"MEH: $i with $e (cause: e.getCause())")
+              e.printStackTrace()
               download(retries - 1)
           }
         download(3)
